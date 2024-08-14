7602047fceb1770256313759a2f2cb7dbea43abf
Seungho1201 <ohseungho1201@gmail.com>
2024-08-14 15:01:25 +0900

    펜 구조체 및 그리기 기능 구현

---
+++ Ref_GuestBook/drawline.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "framework.h"
+#include "Pen_Str.h"
+
+// 전역변수 정의
+vector<PEN_INFO> penMemory;
+PEN_INFO g_Pen_Info;
+
+// 펜 기본 굵기 10
+int pen_Width = 10;
+// 펜 기본색상 BLACK
+COLORREF pen_Color = RGB(0, 0, 0);
+
+
+
+void drawLine(HWND hWnd, UINT message, LPARAM lParam)
+{
+    static int preX, preY;
+    static bool drawStart = false;
+    int x, y;
+    HDC hdc;
+    HPEN myP, osP;
+
+    x = LOWORD(lParam);
+    y = HIWORD(lParam);
+    hdc = GetDC(hWnd);
+
+    myP = CreatePen(PS_SOLID, pen_Width, pen_Color);
+    osP = (HPEN)SelectObject(hdc, myP);
+
+    switch (message)
+    {
+    case WM_LBUTTONDOWN:
+        drawStart = true;
+
+        MoveToEx(hdc, x, y, NULL);
+        LineTo(hdc, x, y);
+
+        preX = x;
+        preY = y;
+
+        // 각 LBUTTON state 별 데이터 구조체에 저장
+        g_Pen_Info.penCoordinate = lParam;     
+        g_Pen_Info.penWidth = pen_Width;
+        g_Pen_Info.penColor = pen_Color;
+        g_Pen_Info.penTime = (DWORD)GetTickCount64();
+        g_Pen_Info.penTime = (DWORD)GetTickCount64();
+        g_Pen_Info.penState = message;
+
+        // 벡터 변수에 위 구조체 데이터 PUSH
+        penMemory.push_back(g_Pen_Info);
+        break;
+
+    case WM_MOUSEMOVE:
+
+        if (drawStart)  
+        {
+            MoveToEx(hdc, preX, preY, NULL);
+            LineTo(hdc, x, y);
+
+            preX = x;
+            preY = y;
+
+            // 각 LBUTTON state 별 데이터 구조체에 저장
+            g_Pen_Info.penCoordinate = lParam;
+            g_Pen_Info.penWidth = pen_Width;
+            g_Pen_Info.penColor = pen_Color;
+            g_Pen_Info.penTime = (DWORD)GetTickCount64();
+            g_Pen_Info.penTime = (DWORD)GetTickCount64();
+            g_Pen_Info.penState = message;
+
+            // 벡터 변수에 위 구조체 데이터 PUSH
+            penMemory.push_back(g_Pen_Info);
+            break;
+        }
+        break;
+
+    case WM_LBUTTONUP:
+        if (drawStart)
+        {
+            // 각 LBUTTON state 별 데이터 구조체에 저장
+            g_Pen_Info.penCoordinate = lParam;
+            g_Pen_Info.penWidth = pen_Width;
+            g_Pen_Info.penColor = pen_Color;
+            g_Pen_Info.penTime = (DWORD)GetTickCount64();
+            g_Pen_Info.penTime = (DWORD)GetTickCount64();
+            g_Pen_Info.penState = message;
+
+            // 벡터 변수에 위 구조체 데이터 PUSH
+            penMemory.push_back(g_Pen_Info);
+
+            drawStart = false;
+            break;
+        }
+        drawStart = false;
+        break;
+    }
+
+    SelectObject(hdc, osP);
+    DeleteObject(myP);  // 펜을 삭제
+
+    ReleaseDC(hWnd, hdc);  // HDC 자원 해제
+}
