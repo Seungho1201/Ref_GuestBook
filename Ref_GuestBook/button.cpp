93d3ab0edbb926e23077a314d7e7388db66d1b76
Seungho1201 <ohseungho1201@gmail.com>
2024-08-19 23:55:56 +0900

    버튼 아이콘 할당기능 추가

--- Ref_GuestBook/button.cpp
+++ Ref_GuestBook/button.cpp
@@ -1,31 +1,66 @@
 #include "Button.h"
 
+extern HWND g_Hwnd;
+
 // 생성자에 이미지 경로 변수 추가 예정
-MakeButton::MakeButton(int x, int y, int weight, int height, int func, LPCWSTR text) {
+MakeButton::MakeButton(int x, int y, int width, int height, int func, LPCWSTR text) {
     this->x = x;
     this->y = y;
-    this->weight = weight;
+    this->width = width;
     this->height = height;
-    this->c_Hwnd = NULL;
     this->func = func;
     this->text = text;
     // 이미지 경로 변수 추가 예정
 }
 
-void MakeButton::mkButton(HWND g_Hwnd) {
-    this->c_Hwnd = g_Hwnd;
-
+/// <summary>
+/// 버튼 구현은 두가지로 나뉜다
+/// 1. 텍스트로 버튼 구현 - 인자 값 X
+/// 2. 파일 경로 상수로 구현 - 아이콘 인자 값 1개
+/// 
+/// 만일 아이콘을 추가하거나 수정하고픈 팀원이 있을 수 있으니 
+/// 아이콘은 비트맵 확장자(bmp)의 파일이여야 하고 "리소스 폴더" 안에 존재해야 함
+/// 아이콘은 Ref_GuestBook.rc 파일에서 정의 한다 (ex: IDI_ERASE_ICON    ICON    "..\..\icon\Pen.ico")
+/// rc 파일에서 정의 후 Resource.h 파일에서 상수로 재정의 한다 (ex: #define IDI_ERASE_ICON     130)
+/// 이제 아이콘 이미지를 넣을 인스턴트의 mkButton(int path) 메서드의 인자로 Resource.h 파일에서 정의한 상수를 넣는다.
+/// </summary>
+void MakeButton::mkButton() {
     CreateWindow(
         L"BUTTON",                                              // 버튼 클래스 이름
         text,                                                   // 버튼 텍스트
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // 버튼 스타일
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,     // 버튼 스타일
         x,                                                      // 버튼의 x 위치
         y,                                                      // 버튼의 y 위치
-        weight,                                                 // 버튼의 폭
+        width,                                                  // 버튼의 폭
         height,                                                 // 버튼의 높이
-        c_Hwnd,                                                 // 부모 윈도우 핸들
+        g_Hwnd,                                                 // 부모 윈도우 핸들
         (HMENU)func,                                            // 버튼의 ID
-        (HINSTANCE)GetWindowLongPtr(c_Hwnd, GWLP_HINSTANCE),    // 인스턴스 핸들
+        (HINSTANCE)GetWindowLongPtr(g_Hwnd, GWLP_HINSTANCE),    // 인스턴스 핸들
         NULL                                                    // 추가 매개변수
     );
+}
+
+void MakeButton::mkButton(int path) {
+    CreateWindow(
+        L"BUTTON",                                              // 버튼 클래스 이름
+        text,                                                   // 버튼 텍스트
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_ICON,           // 버튼 스타일
+        x,                                                      // 버튼의 x 위치
+        y,                                                      // 버튼의 y 위치
+        width,                                                  // 버튼의 폭
+        height,                                                 // 버튼의 높이
+        g_Hwnd,                                                 // 부모 윈도우 핸들
+        (HMENU)func,                                            // 버튼의 ID
+        (HINSTANCE)GetWindowLongPtr(g_Hwnd, GWLP_HINSTANCE),    // 인스턴스 핸들
+        NULL                                                    // 추가 매개변수
+    );
+
+    insertIconImg(text, path, (HINSTANCE)GetWindowLongPtr(g_Hwnd, GWLP_HINSTANCE));
+}
+
+// 아이콘 삽입 메서드
+void MakeButton::insertIconImg(LPCWSTR text, int path, HINSTANCE hInst)
+{
+    HICON hIcon = LoadIcon(hInst, MAKEINTRESOURCE(path));
+    SendMessageW(FindWindowExW(g_Hwnd, NULL, L"button", text), BM_SETIMAGE, IMAGE_ICON, (LPARAM)hIcon);
 }
--- Ref_GuestBook/button.h
+++ Ref_GuestBook/button.h
@@ -6,21 +6,25 @@ class MakeButton {
 private:
     int x;                // 버튼의 x 위치
     int y;                // 버튼의 y 위치
-    int weight;           // 버튼의 폭
+    int width;            // 버튼의 폭
     int height;           // 버튼의 높이
     int func;             // 버튼의 ID
     LPCWSTR text;         // 버튼 텍스트
-    HWND c_Hwnd;          // 부모 윈도우 핸들
 
 public:
-
     /**
     * @brief       버튼 구현 생성자 (x, y, 너비, 높이, 기능, 버튼 안 텍스트)
     */
-    MakeButton(int x, int y, int weight, int height, int func, LPCWSTR text);
+    MakeButton(int x, int y, int width, int height, int func, LPCWSTR text);
+
 
     // 버튼 생성 메서드
-    // 전역 변수로 생성 된 g_Hwnd 변수를 사용해야 하지만 
-    // WM_CREATE 부분에서 초기화가 들어가니 인자로  HWND 타입 변수 사용
-    void mkButton(HWND g_Hwnd);
+    // g_Hwnd 전역 변수를 선언함으로인해 인자 X
+    void mkButton();
+
+    // 이미지 경로 상수 인자값
+    void mkButton(int path);
+
+private:
+    void insertIconImg(LPCWSTR text, int path, HINSTANCE hInst);
 };
